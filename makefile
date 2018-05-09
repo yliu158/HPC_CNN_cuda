388f6f08ee1041e808bc483a4d3c55611b5a12b0
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-09 15:13:25 -0400

    update device.cu

--- makefile
+++ makefile
@@ -4,7 +4,7 @@ serial.o: serial.cpp main.h
 	g++ -std=c++17 -c serial.cpp main.h
 
 device.o: device.cu main.h
-	nvcc -c fclforward.cu
+	nvcc -c device.cu
 
 link:
 	nvcc device.o serial.o -o test
--- serial.cpp
+++ serial.cpp
@@ -1235,7 +1235,8 @@ FullyConnectedLayer<IN_DIMS, N_NEURONS>::check_downstream_derivative(const int l
 }
 
 template <typename IN_DIMS, size_t N_NEURONS>
-int full_forward_device();
+int full_forward_device(const Input &input, const Array<Input, N_NEURONS> &weight, const Array<double, N_NEURONS> &bias,
+ const Array<double, N_NEURONS> &dropped, Output &output);
 
 
 template <typename IN_DIMS, size_t N_NEURONS>
