6efd3636451c5bac66d7f255bf471e18b98e1144
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-09 23:46:12 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -1265,7 +1265,8 @@ FullyConnectedLayer<IN_DIMS, N_NEURONS>::forward(const Input &input, const Array
     }
 
     // cuda method
-    input.begin();
+    printf(typeof(input.begin()));
+    full_device_forward(weight.begin(), input.begin(), output.begin());
 }
 
 
