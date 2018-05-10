b67db4bbe3d3ba2f6e3203d54fdf3593eec86629
Yang Liu <yangliu@Yangs-MacBook-Pro.local>
2018-05-10 13:20:30 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -1711,7 +1711,7 @@ main() {
     in = (double*)malloc(sizeof(double)*28*28*32);
     out = (double*)malloc(sizeof(double)*14*14*32);
     for (int i = 0; i < 28*28*32; ++i) {
-      in[i] = (double)(rand()%2);
+      in[i] = (double)(rand()%5);
       if (i%28 == 0)printf("\n");
       if (i%(28*28) == 0) printf("\n");
       printf("%lf ", in[i]);
@@ -1719,12 +1719,14 @@ main() {
     }
 
     pool_device_forward(in, out);
+
     for (int i = 0; i < 14*14*32; ++i) {
       if (i%14 == 0) printf("\n");
+      if (i%(14*14) == 0) printf("\n");
       printf("%lf ", out[i]);
     }
-    free(in);
-    free(out);
+    // free(in);
+    // free(out);
 }
 
 
