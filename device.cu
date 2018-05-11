da072c7a6a6514e66b07882797dc67b2e06dba58
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-11 14:39:51 -0400

    update

--- device.cu
+++ device.cu
@@ -89,12 +89,13 @@ __global__ void conv_forward_all(double* in, double* filter, double* bias, doubl
   int z_in = blockIdx.x*(blockDim.x+4)*(blockDim.y+4);
   int w_in = blockIdx.y*gridDim.x*(blockDim.x+4)*(blockDim.y+4);
   int i_id = x_in + y_in + z_in + w_in;
-  printf("out: (%d, %d, %d, %d) in: (%d, %d, %d, %d)\n", x_out, y_out, z_out, w_out, x_in, y_in, z_in, w_in);
-  for (int i = -2; i <= 2; ++i) {
-    for (int j = -2; j <= 2; ++j) {
-      out[o_id+i*blockDim.x+j] += filter[(i+2)*5+j+2] * in[i_id+i*(blockDim.x+4)+j+2];
-    }
-  }
+  // printf("out: (%d, %d, %d, %d) in: (%d, %d, %d, %d)\n", x_out, y_out, z_out, w_out, x_in, y_in, z_in, w_in);
+  // for (int i = -2; i <= 2; ++i) {
+  //   for (int j = -2; j <= 2; ++j) {
+  //     out[o_id+i*blockDim.x+j] += filter[(i+2)*5+j+2] * in[i_id+i*(blockDim.x+4)+j+2];
+  //   }
+  // }
+  printf("value:%lf  i_id:%d\n", in[i_id], i_id);
 }
 
 void conv_forward_device(double* in, double* filter, double* bias, double* out, size_t size, size_t img_d, size_t fil_d) {
