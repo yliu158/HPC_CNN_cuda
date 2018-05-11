f8e24ae069e01a0b0f5d2871f1f0ea9ef5b30776
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-11 00:26:20 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -1758,7 +1758,7 @@ main() {
     for (int i = 0; i < 32; ++i) {
       for (int j = 0; j < 32; ++j) {
         in[i*32+j] = (double)(rand()%2);
-        printf("%lf", in[i*32+j]);
+        printf("%lf  ", in[i*32+j]);
       }
       printf("\n");
     }
@@ -1767,7 +1767,7 @@ main() {
       for (int j = 0; j < 5; ++j) {
         for (int k = 0; k < 5; ++k) {
           filter[i*5*5+j*5+k] = (double)(rand()%2);
-          printf("%lf", filter[i*5*5+j*5+k]);
+          printf("%lf  ", filter[i*5*5+j*5+k]);
         }
         printf("\n");
       }
@@ -1776,14 +1776,14 @@ main() {
     printf("\n");
     for (int i = 0; i < 32; ++i) {
       bias[i] = (double)(rand()%2);
-      printf("%lf", bias[i]);
+      printf("%lf  ", bias[i]);
     }
     printf("\n");
     conv_forward_device_first(in, filter, bias, out);
     for (int i = 0; i < 32; ++i) {
       for (int j = 0; j < 28; ++j) {
         for (int k = 0; k < 28; ++k) {
-          printf("%lf", out[i*28*28+j*28+k]);
+          printf("%lf  ", out[i*28*28+j*28+k]);
         }
         printf("\n");
       }
