5d261bf39f1538a3a1f6a108df18b06d389b07ef
Yang Liu <yangliu@Yangs-MacBook-Pro.local>
2018-05-11 18:31:41 -0400

    update

--- device.cu
+++ device.cu
@@ -53,7 +53,7 @@ __global__ void pool_forward_all(double *in, double *out, size_t size_out) {
   //     }
   //   }
   // }
-  printf("Hello o_id:\n", o_id);
+  printf("Hello o_id:\n", threadIdx.x);
 }
 
 void pool_forward_device(double* in, double* out, size_t size_out, size_t img_d) {
