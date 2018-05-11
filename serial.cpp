37043457da29c2982ef543757734d5e14340592c
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-11 10:56:05 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -1750,24 +1750,65 @@ main() {
 
     // full_forward_device();
     // run3();
+    // double *in, *filter, *bias, *out;
+    // in = (double*)malloc(sizeof(double)*32*32*1);
+    // filter = (double*)malloc(sizeof(double)*5*5*32);
+    // bias = (double*)malloc(sizeof(double)*32);
+    // out = (double*)malloc(sizeof(double)*28*28*1);
+    // for (int i = 0; i < 32; ++i) {
+    //   for (int j = 0; j < 32; ++j) {
+    //     in[i*32+j] = (double)(rand()%2);
+    //     printf("%lf  ", in[i*32+j]);
+    //   }
+    //   printf("\n");
+    // }
+    // printf("\n");
+    // for (int i = 0; i < 32; ++i) {
+    //   for (int j = 0; j < 5; ++j) {
+    //     for (int k = 0; k < 5; ++k) {
+    //       filter[i*5*5+j*5+k] = (double)(rand()%2);
+    //       printf("%lf  ", filter[i*5*5+j*5+k]);
+    //     }
+    //     printf("\n");
+    //   }
+    //   printf("\n");
+    // }
+    // printf("\n");
+    // for (int i = 0; i < 32; ++i) {
+    //   bias[i] = (double)(rand()%2);
+    //   printf("%lf  ", bias[i]);
+    // }
+    // printf("\n");
+    // conv_forward_device_first(in, filter, bias, out);
+    // for (int i = 0; i < 32; ++i) {
+    //   for (int j = 0; j < 28; ++j) {
+    //     for (int k = 0; k < 28; ++k) {
+    //       printf("%lf  ", out[i*28*28+j*28+k]);
+    //     }
+    //     printf("\n");
+    //   }
+    //   printf("\n");
+    // }
+    // printf("\n");
+
     double *in, *filter, *bias, *out;
-    in = (double*)malloc(sizeof(double)*32*32*1);
-    filter = (double*)malloc(sizeof(double)*5*5*32);
+    in = (double*)malloc(sizeof(double)*5*5*1);
+    filter = (double*)malloc(sizeof(double)*3*3*32);
     bias = (double*)malloc(sizeof(double)*32);
-    out = (double*)malloc(sizeof(double)*28*28*1);
-    for (int i = 0; i < 32; ++i) {
-      for (int j = 0; j < 32; ++j) {
-        in[i*32+j] = (double)(rand()%2);
-        printf("%lf  ", in[i*32+j]);
+    out = (double*)malloc(sizeof(double)*3*3*32);
+    for (int i = 0; i < 5; ++i) {
+      for (int j = 0; j < 5; ++j) {
+        in[i*5+j] = (double)(rand()%2);
+        printf("%lf  ", in[i*5+j]);
       }
       printf("\n");
     }
     printf("\n");
     for (int i = 0; i < 32; ++i) {
-      for (int j = 0; j < 5; ++j) {
-        for (int k = 0; k < 5; ++k) {
-          filter[i*5*5+j*5+k] = (double)(rand()%2);
-          printf("%lf  ", filter[i*5*5+j*5+k]);
+      for (int j = 0; j < 3; ++j) {
+        for (int k = 0; k < 3; ++k) {
+          filter[i*3*3+j*3+k] = (double)(rand()%2);
+          printf("%lf  ", filter[i*3*3+j*3+k]);
         }
         printf("\n");
       }
@@ -1779,11 +1820,11 @@ main() {
       printf("%lf  ", bias[i]);
     }
     printf("\n");
-    conv_forward_device_first(in, filter, bias, out);
+    conv_forward_device_test(in, filter, bias, out);
     for (int i = 0; i < 32; ++i) {
-      for (int j = 0; j < 28; ++j) {
-        for (int k = 0; k < 28; ++k) {
-          printf("%lf  ", out[i*28*28+j*28+k]);
+      for (int j = 0; j < 3; ++j) {
+        for (int k = 0; k < 3; ++k) {
+          printf("%lf  ", out[i*3*3+j*3+k]);
         }
         printf("\n");
       }
@@ -1820,7 +1861,11 @@ main() {
     //   printf("%lf  ", bias[i]);
     // }
     // printf("\n");
-    conv_forward_device_first(in, filter, bias, out);
+
+
+    // conv_forward_device_first(in, filter, bias, out);
+
+
     // for (int i = 0; i < 32; ++i) {
     //   for (int j = 0; j < 28; ++j) {
     //     for (int k = 0; k < 28; ++k) {
