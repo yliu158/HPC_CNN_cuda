709335da9d4eec949559b6677d316558d34c54a9
Yang Liu <yangliu@Yangs-MacBook-Pro.local>
2018-05-11 18:31:12 -0400

    update

--- device.cu
+++ device.cu
@@ -44,15 +44,15 @@
 // }
 
 __global__ void pool_forward_all(double *in, double *out, size_t size_out) {
-  int o_id = threadIdx.x + threadIdx.y*blockDim.x + blockIdx.x*blockDim.x*blockDim.y;
-  int i_id = threadIdx.x*2 + threadIdx.y*2*blockDim.x*2 + blockIdx.x*blockDim.x*2*blockDim.y*2;
-  for (size_t i = 0; i < 2; i++) {
-    for (size_t j = 0; j < 2; j++) {
-      if (out[o_id] < in[i_id+i*2*threadIdx.x+j]) {
-        out[o_id] = in[i_id+i*2*threadIdx.x+j];
-      }
-    }
-  }
+  // int o_id = threadIdx.x + threadIdx.y*blockDim.x + blockIdx.x*blockDim.x*blockDim.y;
+  // int i_id = threadIdx.x*2 + threadIdx.y*2*blockDim.x*2 + blockIdx.x*blockDim.x*2*blockDim.y*2;
+  // for (size_t i = 0; i < 2; i++) {
+  //   for (size_t j = 0; j < 2; j++) {
+  //     if (out[o_id] < in[i_id+i*2*threadIdx.x+j]) {
+  //       out[o_id] = in[i_id+i*2*threadIdx.x+j];
+  //     }
+  //   }
+  // }
   printf("Hello o_id:\n", o_id);
 }
 
