98bf07fb3ccd52163d097deb2dfab79e14068db0
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-12 15:46:19 -0400

    update

--- device.cu
+++ device.cu
@@ -5,8 +5,8 @@ __global__ void pool_forward(double *in, double *out) {
   int i_id = threadIdx.x*2 + threadIdx.y*2*blockDim.x*2 + blockIdx.x*blockDim.x*2*blockDim.y*2;
   for (size_t i = 0; i < 2; i++) {
     for (size_t j = 0; j < 2; j++) {
-      if (out[o_id] < in[i_id+i*2*size_out+j]) {
-        out[o_id] = in[i_id+i*2*size_out+j];
+      if (out[o_id] < in[i_id+i*2*blockDim.x+j]) {
+        out[o_id] = in[i_id+i*2*blockDim.x+j];
       }
     }
   }
