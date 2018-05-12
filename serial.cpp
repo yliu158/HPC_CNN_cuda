81beb1657a46d313f2ed200e9e0339838e21d1ce
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-12 11:14:56 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -869,7 +869,11 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::forward(const Input &input, const Filter
     for (int i = 0; i < N_FILTERS; ++i) {
       for (int k = 0; k < IN_H; ++k) {
         for (int j = 0; j < IN_W; ++j) {
-          assert(output[i][k][j] == d_out[i][k][j]);
+          // assert(output[i][k][j] == d_out[i][k][j]);
+          if (output[i][k][j] != d_out[i][k][j]) {
+            printf("\n\n%lf  %lf\n", output[i][k][j], d_out[i][k][j]);
+            break;
+          }
           printf("%lf ", d_out[i][k][j]);
         }
         printf("\n" );
