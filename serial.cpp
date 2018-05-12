a0c00350f45ddf56fa3cab9a9887f060ee434cdf
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-12 11:56:16 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -835,28 +835,28 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::forward(const Input &input, const Filter
 
 
 
-    // Output d_out;
-    // conv_forward_device((double*)&in_padded[0][0][0], (double*)&filter[0][0][0][0], (double*)&bias[0],(double*)&d_out[0][0][0], IN_H, IN_D, N_FILTERS);
+    Output d_out;
+    conv_forward_device((double*)&in_padded[0][0][0], (double*)&filter[0][0][0][0], (double*)&bias[0],(double*)&d_out[0][0][0], IN_H, IN_D, N_FILTERS);
 
-    // for (int i = 0; i < N_FILTERS; ++i) {
-    //   for (int k = 0; k < IN_H; ++k) {
-    //     for (int j = 0; j < IN_W; ++j) {
-    //       // assert(output[i][k][j] == d_out[i][k][j]); Precision different
-    //       printf("%lf ", d_out[i][k][j]);
-    //     }
-    //     printf("\n" );
-    //   }
-    //   printf("\n" );
-    //
-    //   for (size_t k = 0; k < IN_H; k++) {
-    //     for (size_t j = 0; j < IN_W; j++) {
-    //       printf("%lf ", output[i][k][j]);
-    //     }
-    //     printf("\n" );
-    //   }
-    //   printf("\n" );
-    //   printf("======================================================\n" );
-    // }
+    for (int i = 0; i < N_FILTERS; ++i) {
+      for (int k = 0; k < IN_H; ++k) {
+        for (int j = 0; j < IN_W; ++j) {
+          // assert(output[i][k][j] == d_out[i][k][j]); Precision different
+          printf("%lf ", d_out[i][k][j]);
+        }
+        printf("\n" );
+      }
+      printf("\n" );
+
+      for (size_t k = 0; k < IN_H; k++) {
+        for (size_t j = 0; j < IN_W; j++) {
+          printf("%lf ", output[i][k][j]);
+        }
+        printf("\n" );
+      }
+      printf("\n" );
+      printf("======================================================\n" );
+    }
     // exit(1);
 
 
@@ -1291,9 +1291,6 @@ template <typename IN_DIMS, size_t N_NEURONS>
 void
 FullyConnectedLayer<IN_DIMS, N_NEURONS>::forward(const Input &input, const Array<Input, N_NEURONS> &weight, const Array<double, N_NEURONS> &bias,
  const Array<double, N_NEURONS> &dropped, Output &output) {
-
-   printf("N_NEURONS: %d   IN_D: %d\n", N_NEURONS, IN_D);
-   exit(1);
 
     for (size_t i = 0; i < N_NEURONS; i++) {
         double &out(output[0][0][i]);
