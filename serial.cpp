6bbf0461e68e659ab09511fc92f2d15f98af015b
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-12 10:46:29 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -854,23 +854,11 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::forward(const Input &input, const Filter
 
     Output d_out;
     conv_forward_device((double*)&in_padded[0][0][0], (double*)&filter[0][0][0][0], (double*)&bias[0],(double*)&d_out[0][0][0], IN_H, IN_D, N_FILTERS);
-    for (int i = 0; i < N_FILTERS; ++i) {
-      for (int k = 0; k < IN_H; ++k) {
-        for (int j = 0; j < IN_W; ++j) {
-          assert(output[i][k][j] == d_out[i][k][j]);
-          // printf("%lf ", d_out[i][k][j]);
-        }
-        printf("\n" );
-      }
-      printf("\n" );
-    }
-    printf("\n" );
-    //
     // for (int i = 0; i < N_FILTERS; ++i) {
     //   for (int k = 0; k < IN_H; ++k) {
     //     for (int j = 0; j < IN_W; ++j) {
-    //       // assert(output[i][k][j] == d_out[i][k][j]);
-    //       printf("%lf ", output[i][k][j]);
+    //       assert(output[i][k][j] == d_out[i][k][j]);
+    //       // printf("%lf ", d_out[i][k][j]);
     //     }
     //     printf("\n" );
     //   }
@@ -878,15 +866,27 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::forward(const Input &input, const Filter
     // }
     // printf("\n" );
 
-    // for (size_t i = 0; i < N_FILTERS; i++) {
-    //   for (size_t k = 0; k < IN_H; k++) {
-    //     for (size_t j = 0; j < IN_W; j++) {
-    //       printf("%lf\t", output[i][k][j]);
-    //     }
-    //     printf("\n" );
-    //   }
-    //   printf("\n" );
-    // }
+    for (int i = 0; i < N_FILTERS; ++i) {
+      for (int k = 0; k < IN_H; ++k) {
+        for (int j = 0; j < IN_W; ++j) {
+          // assert(output[i][k][j] == d_out[i][k][j]);
+          printf("%lf ", d_out[i][k][j]);
+        }
+        printf("\n" );
+      }
+      printf("\n" );
+    }
+    printf("\n" );
+
+    for (size_t i = 0; i < N_FILTERS; i++) {
+      for (size_t k = 0; k < IN_H; k++) {
+        for (size_t j = 0; j < IN_W; j++) {
+          printf("%lf\t", output[i][k][j]);
+        }
+        printf("\n" );
+      }
+      printf("\n" );
+    }
     exit(1);
 }
 
