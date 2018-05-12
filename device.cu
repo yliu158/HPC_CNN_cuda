350061b0361e9eed24842af1dad4bf700db6b51e
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-12 15:50:50 -0400

    update

--- device.cu
+++ device.cu
@@ -16,15 +16,15 @@ void pool_forward_device(double* in, double* out, size_t size_out, size_t img_d)
   double *d_in, *d_out;
   cudaMalloc((double**)&d_in, sizeof(double)*size_out*2*size_out*2*img_d);
   cudaMalloc((double**)&d_out, sizeof(double)*size_out*size_out*img_d);
-  cudaMemcpy(d_in, in, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMemcpy(d_in, in, sizeof(double)*size_out*2*size_out*2*img_d, cudaMemcpyHostToDevice);
 
   // dim3 block_size(size_out, size_out, 1);
   // dim3 grid_size(img_d, 1, 1);
   // pool_forward<<<grid_size, block_size>>>(d_in, d_out);
   //
   // cudaMemcpy(out, d_out, sizeof(double)*size_out*size_out*img_d, cudaMemcpyDeviceToHost);
-  // cudaFree(d_in);
-  // cudaFree(d_out);
+  cudaFree(d_in);
+  cudaFree(d_out);
 }
 
 
