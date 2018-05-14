c29b99a1194897dbed8e58de86d525e2f90fa6b2
Yang Liu <yangliu@Yangs-MacBook-Pro.local>
2018-05-14 10:44:57 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -558,20 +558,13 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
     for (size_t i = 0; i < N_FILTERS; i++) {
       for (size_t j = 0; j < IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
-          assert (this->downstream_deriv[i][j][k] == d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]);
-          // printf("%lf ", this->downstream_deriv[i][j][k]);
+          if (this->downstream_deriv[i][j][k] != d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]){
+            printf("%lf   %lf\n", this->downstream_deriv[i][j][k], d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]);
+            exit(1);
+          }
         }
-        // printf("\n");
-      //   for (size_t k = 0; k < IN_W; k++) {
-      //     // assert (this->downstream_deriv[i][j][k] == d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]);
-      //     printf("%lf ", d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]);
-      //   }
-      //   printf("   mine\n");
       }
-      // printf("=============================================================================\n");
     }
-    // printf("\n");
-    // exit(1);
     this->previous_layer->backprop(this->downstream_deriv, mb_size);
 }
 
