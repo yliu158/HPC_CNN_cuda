351c255605420a466b87e302e9f63e8f2b535a6c
Yang Liu <yangliu@Yangs-MacBook-Pro.local>
2018-05-14 13:11:32 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -564,12 +564,12 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
       for (size_t j = 0; j < IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
           // printf("%lf  \n", this->downstream_deriv[i][j][k]);
-          if (this->downstream_deriv[i][j][k] != 0) printf("%lf  \n", this->downstream_deriv[i][j][k]);
+          if (this->downstream_deriv[i][j][k] != 0) printf("serial: %lf\n", this->downstream_deriv[i][j][k]);
         }
       }
       for (size_t j = 0; j < IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
-          if (d_down_deriv[k+j*IN_W+ i*IN_W*IN_H] != 0) printf("%lf\n",  d_down_deriv[k+j*IN_W+ i*IN_W*IN_H] != 0);
+          if (d_down_deriv[k+j*IN_W+ i*IN_W*IN_H] != 0) printf("paralle%lf\n",  d_down_deriv[k+j*IN_W+ i*IN_W*IN_H] != 0);
         }
       }
     }
