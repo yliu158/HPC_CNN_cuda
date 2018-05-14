4c8c4f3bbf4b3cac9eab3143361184bc7dbb9ba9
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-14 00:26:10 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -527,7 +527,7 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
     for (size_t i = 0; i <IN_D; i++) {
       for (size_t j = 0; j <IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
-          printf("%lf  ", downstream_deriv[i][j][k]);
+          printf("%lf  ", this->downstream_deriv[i][j][k]);
         }
         printf("\n");
       }
