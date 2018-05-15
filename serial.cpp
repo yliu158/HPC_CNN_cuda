b204c693aef69c92316cff125a741764c25eee25
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-15 18:33:37 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -544,11 +544,8 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
         }
       }
     }
-    conv_backprop_downstream_device((double*)&this->output[0][0][0], (double*)&this->downstream_deriv[0][0][0],
+    conv_backprop_downstream_device((double*)&this->output[0][0][0], d_down_deriv,
   (double*)&upstream_deriv[0][0][0], (double*)&m_filter[0][0][0], IN_H, IN_D, N_FILTERS);
-    // conv_backprop_device((double*)&input[0][0][0], (double*)&this->output[0][0][0], d_down_deriv,
-    // (double*)&upstream_deriv[0][0][0], (double*)&m_filter_deriv[0][0][0], (double*)&m_filter[0][0][0], (double*)&m_bias_deriv[0],
-    // IN_H, IN_D, N_FILTERS);
     for (size_t i = 0; i < IN_D; i++) {
       for (size_t j = 0; j < IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
@@ -556,7 +553,7 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
         }
       }
     }
-    exit(1);
+    // exit(1);
     // ***********************************************************************//
     this->previous_layer->backprop(this->downstream_deriv, mb_size);
 }
