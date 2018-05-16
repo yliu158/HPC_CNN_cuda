3d600f08918c8f6915a5a16d47f1283fcf3b65d2
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-15 23:02:34 -0400

    update

--- device.cu
+++ device.cu
@@ -73,7 +73,7 @@ __global__ void conv_backprop_down_deriv_sum(double* d_down_deriv_tmp, double* d
   for (size_t i = 0; i < fil_d; i++) {
     // d_down_deriv[id] += d_down_deriv_tmp[id+i*offset];
   }
-  printf("Hello\n" );
+  // printf("Hello\n" );
 }
 
 void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, double* filter, size_t size, size_t img_d, size_t fil_d) {
