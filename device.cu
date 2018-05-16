f49eb5807206a38d8065152cc8bea19cc8690545
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-16 00:18:39 -0400

    update

--- device.cu
+++ device.cu
@@ -64,7 +64,7 @@ __global__ void conv_backprop_down_deriv(double* down_deriv, double* filter, dou
       }
     }
   }
-  printf("id: %d  threadIdx.x: %d threadIdx.y %d  blockIdx.x %d  blockIdx.y %d\n", id, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y);
+  printf("id: %d  threadIdx.x: %d threadIdx.y %d  blockIdx.x %d  blockIdx.y %d\n", d_id, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y);
   __syncthreads();
   // printf(" blockDim.x:%d, blockDim.y:%d, gridDim.x:%d, gridDim.y:%d, threadIdx.x:%d, threadIdx.y:%d, blockIdx.x:%d, blockIdx.y:%d\n", blockDim.x, blockDim.y, gridDim.x, gridDim.y, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y);
 }
