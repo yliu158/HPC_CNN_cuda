76ff0c09f63782588f4bdb02a51d4d62a693c8fe
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-15 23:57:39 -0400

    update

--- device.cu
+++ device.cu
@@ -91,8 +91,8 @@ void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, doubl
   dim3 grid_size_d(img_d, fil_d, 1);
   dim3 grid_size_s(img_d, 1,1);
   dim3 block_size_s(size+4, size+4, 1);
-  conv_backprop_down_deriv<<<grid_size_d, block_size_d>>>(d_down_deriv_tmp, d_filter, d_up_deriv);
-  conv_backprop_down_deriv_sum<<<grid_size_s, 10>>>(d_down_deriv_tmp, down_deriv, fil_d);
+  // conv_backprop_down_deriv<<<grid_size_d, block_size_d>>>(d_down_deriv_tmp, d_filter, d_up_deriv);
+  conv_backprop_down_deriv_sum<<<grid_size_s, block_size_s>>>(d_down_deriv_tmp, down_deriv, fil_d);
 
   cudaMemcpy(down_deriv, d_down_deriv, sizeof(double)*(size+4)*(size+4)*img_d, cudaMemcpyDeviceToHost);
   cudaFree(d_down_deriv_tmp);
