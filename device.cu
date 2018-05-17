e768234163c75c16c79dfcbfd33a6abdd468ad3d
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-17 11:12:55 -0400

    update

--- device.cu
+++ device.cu
@@ -185,7 +185,7 @@ void pool_backprop_device(double *down_deriv, double *up_deriv, int *max_i, int 
 //   cudaFree(d_bias_deriv);
 // }
 
-__global__ void conv_backprop_downstream_deriv(double* down_deriv, double* up_deriv, double* filter, const size_t size) {
+__global__ void conv_backprop_downstream_deriv(double* down_deriv, double* up_deriv, double* filter, size_t size) {
   // printf("blockDim.x: %d, blockDim.y: %d, gridDim.x: %d, gridDim.y: %d\n", blockDim.x, blockDim.y, gridDim.x, gridDim.y);
   __shared__ double share_dd[28*28];
   size_t u_id = blockIdx.x*size*size + threadIdx.y*size + threadIdx.x;
@@ -235,6 +235,58 @@ void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, doubl
   cudaFree(d_down_deriv);
   cudaFree(d_up_deriv);
   cudaFree(d_filter);
+}
+
+__global__ void conv_backprop_filter_deriv(double* filter_deriv, double* up_deriv, double* input, size_t size, double mb_size) {
+  // printf("blockDim.x: %d, blockDim.y: %d, gridDim.x: %d, gridDim.y: %d\n", blockDim.x, blockDim.y, gridDim.x, gridDim.y);
+  __shared__ double share_fd[5*5];
+  size_t u_id = blockIdx.x*size*size + threadIdx.y*size + threadIdx.x;
+  size_t f_id = blockIdx.x*gridDim.y*5*5 + blockIdx.y*5*5;
+  size_t i_id = blockIdx.y*size*size;
+
+  size_t f_beg_i, f_beg_j, f_end_i, f_end_j;
+  if (-(long long)threadIdx.x + 2LL > 0LL) f_beg_i = -(long long)threadIdx.x + 2LL;
+  else f_beg_i = 0LL;
+  if (-(long long)threadIdx.y + 2LL > 0LL) f_beg_j = -(long long)threadIdx.y + 2LL;
+  else f_beg_j = 0LL;
+  if ((long long)blockDim.x + 2LL - (long long)threadIdx.x < 5LL) f_end_i = (long long)blockDim.x + 2LL - (long long)threadIdx.x;
+  else f_end_i = 5LL;
+  if ((long long)blockDim.y + 2LL - (long long)threadIdx.y < 5LL) f_end_j = (long long)blockDim.y + 2LL - (long long)threadIdx.y;
+  else f_end_j = 5LL;
+
+  for (size_t i = f_beg_i; i < f_end_i; i++) {
+    for (size_t j = f_beg_j; j < f_end_j; j++) {
+      size_t in_i = threadIdx.x + i - 2;
+      size_t in_j = threadIdx.y + j - 2;
+      share_fd[f_id + i*5 + j] += input[i_id + i*size +j] * up_deriv[u_id]/mb_size;
+    }
+  }
+  __syncthreads();
+  for (size_t i = 0; i < size; i++) {
+    for (size_t j = 0; j < size; j++) {
+      down_deriv[d_id + i*size +j] = share_fd[i*5+j];
+    }
+  }
+  // printf("u_id: %d\n", u_id);
+}
+
+
+void conv_backprop_filter_device(double* filter_deriv, double* up_deriv, double* input, size_t size, size_t img_d, size_t fil_d， double mb_size) {
+  double *d_filter_deriv, *d_up_deriv, *d_input;
+  cudaMalloc((double**)&d_filter_deriv, sizeof(double)*5*5*img_d*fil_d);
+  cudaMalloc((double**)&d_up_deriv,sizeof(double)*size*size*fil_d);
+  cudaMalloc((double**)&d_input,sizeof(double)*size*size*img_d);
+  cudaMemcpy(d_up_deriv, up_deriv, sizeof(double)*size*size*fil_d, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_input, input, sizeof(double)*size*size*img_d, cudaMemcpyHostToDevice);
+
+  dim3 block(size, size, 1);
+  dim3 grid(fil_d, img_d, 1);
+  conv_backprop_filter_deriv<<<grid, block>>>(d_filter_deriv, d_up_deriv, d_input, size, mb_size);
+
+  cudaMemcpy(filter_deriv, d_filter_deriv, sizeof(double)*5*5*img_d*fil_d, cudaMemcpyDeviceToHost);
+  cudaFree(d_up_deriv);
+  cudaFree(d_filter);
+  cudaFree(d_input);
 }
 
 __global__ void conv_forward(double* in, double* filter, double* bias, double* out) {
