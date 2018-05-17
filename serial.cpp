1621cbdfae13a1d75fea4f15a0338f98fc50219b
Yang Liu <yangliu@Yangs-MBP.stny.rr.com>
2018-05-17 11:28:18 -0400

    update

--- serial.cpp
+++ serial.cpp
@@ -460,7 +460,8 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
     using ll_t = long long;
 
     this->downstream_deriv = 0;
-
+    // ***********************************************************************//
+    //                         Prepare Data
     double* d_down_deriv = (double*)malloc(sizeof(double)*IN_H*IN_W*IN_D);
     for (size_t i = 0; i < IN_D; i++) {
       for (size_t j = 0; j < IN_H; j++) {
@@ -480,6 +481,10 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
         }
       }
     }
+    //                        END Preparing
+    // ***********************************************************************//
+
+
     //=======================================================================
     // Compute downstream derivatives.  Note that we slide over the output, not the input.  It can probably also
     // be done sliding over the input, but I think it would be significantly harder.
@@ -552,16 +557,7 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
 
     // ***********************************************************************//
     //                            Prove of correctness
-
     conv_backprop_downstream_device(d_down_deriv, (double*)&upstream_deriv[0][0][0], (double*)&m_filter[0][0][0], IN_H, IN_D, N_FILTERS);
-
-    // for (size_t i = 0; i < IN_D; i++) {
-    //   for (size_t j = 0; j < IN_H; j++) {
-    //     for (size_t k = 0; k < IN_W; k++) {
-    //       assert(this->downstream_deriv[i][j][k] == d_down_deriv[k+j*IN_W+ i*IN_W*IN_H]);
-    //     }
-    //   }
-    // }
     for (size_t i = 0; i < IN_D; i++) {
       for (size_t j = 0; j < IN_H; j++) {
         for (size_t k = 0; k < IN_W; k++) {
@@ -581,8 +577,8 @@ ConvolutionalLayer<IN_DIMS, N_FILTERS>::backprop(const Output &upstream_deriv, c
         }
       }
     }
-
-    exit(1);
+    // exit(1);
+    //                          END Proving
     // ***********************************************************************//
     this->previous_layer->backprop(this->downstream_deriv, mb_size);
 }
