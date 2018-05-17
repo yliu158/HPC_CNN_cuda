be2b89f2e29d0ba32ff5f57ca0c98cda7b8cf411
Yang Liu <yangliu@yangs-mbp.home>
2018-05-16 21:52:37 -0400

    update

--- device.cu
+++ device.cu
@@ -77,9 +77,9 @@ __global__ void conv_backprop_down_deriv_sum(double* d_down_deriv_tmp, double* d
 }
 
 void conv_backprop_downstream_device_helper(double* d_down_deriv_tmp, double* d_down_deriv,size_t size, size_t img_d, size_t fil_d) {
-  // dim3 block(size+4, size+4, 1);
-  // dim3 grid(img_d, 1, 1);
-  // conv_backprop_down_deriv_sum<<<grid, block>>>(d_down_deriv_tmp, d_down_deriv, fil_d);
+  dim3 block(size+4, size+4, 1);
+  dim3 grid(img_d, 1, 1);
+  conv_backprop_down_deriv_sum<<<grid, block>>>(d_down_deriv_tmp, d_down_deriv, fil_d);
 }
 
 void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, double* filter, size_t size, size_t img_d, size_t fil_d) {
@@ -94,7 +94,7 @@ void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, doubl
   dim3 block_size_d(size+4, size+4, 1);
   dim3 grid_size_d(img_d, fil_d, 1);
   conv_backprop_down_deriv<<<grid_size_d, block_size_d>>>(d_down_deriv_tmp, d_filter, d_up_deriv);
-  conv_backprop_downstream_device_helper(d_down_deriv_tmp, d_down_deriv, size, img_d, fil_d);
+  // conv_backprop_downstream_device_helper(d_down_deriv_tmp, d_down_deriv, size, img_d, fil_d);
   cudaMemcpy(down_deriv, d_down_deriv, sizeof(double)*(size+4)*(size+4)*img_d, cudaMemcpyDeviceToHost);
   cudaFree(d_down_deriv_tmp);
   cudaFree(d_down_deriv);
