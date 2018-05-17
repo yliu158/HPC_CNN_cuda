f6aa550d8d145afa06089894a508146d14822b82
Yang Liu <yangliu@yangs-mbp.home>
2018-05-16 22:48:17 -0400

    update

--- device.cu
+++ device.cu
@@ -182,8 +182,8 @@ void conv_backprop_downstream_device(double* down_deriv, double* up_deriv, doubl
   cudaMalloc((double**)&d_down_deriv, sizeof(double)*(size+4)*(size+4)*img_d);
   cudaMalloc((double**)&d_up_deriv,sizeof(double)*size*size*fil_d);
   cudaMalloc((double**)&d_filter,sizeof(double)*5*5*img_d*fil_d);
-  cudaMalloc((double**)&d_down_deriv_tmp), sizeof(double)*(size+4)*(size+4)*img_d*fil_d);
-  
+  cudaMalloc((double**)&d_down_deriv_tmp, sizeof(double)*(size+4)*(size+4)*img_d*fil_d);
+
   cudaFree(d_down_deriv);
   cudaFree(d_up_deriv);
   cudaFree(d_filter);
